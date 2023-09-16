71dc8a0d648a159d179e7cc826d270a67cd8864c
DarthCooper <loganbarber1@outlook.com>
2023-09-16 11:52:58 -0600

    Made it a server

---
+++ Assets/LobbyController.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+public class LobbyController : MonoBehaviour
+{
+    public static LobbyController instance;
+
+    public TMP_Text LobbyNameText;
+
+    public GameObject PlayerListViewContent;
+    public GameObject PlayerListItemPrefab;
+    public GameObject LocalPlayerObject;
+
+    public ulong CurrentLobbyID;
+    public bool PlayerItemCreated = false;
+    private List<PlayerListItem> PlayerListItems = new List<PlayerListItem>();
+    public PlayerObjectController LocalplayerController;
+
+    private CustomNetworkManager manager;
+
+    private CustomNetworkManager Manager
+    {
+        get
+        {
+            if (manager != null)
+            {
+                return manager;
+            }
+            return manager = CustomNetworkManager.singleton as CustomNetworkManager;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null) { instance = this; }
+    }
+
+    public void UpdateLobbyName()
+    {
+        CurrentLobbyID = Manager.GetComponent<SteamLobby>().CurrentLobbyID;
+        LobbyNameText.text = SteamMatchmaking.GetLobbyData(new CSteamID(CurrentLobbyID), "name");
+    }
+
+    public void UpdatePlayerList()
+    {
+        if(!PlayerItemCreated) { CreateHostPlayerItem(); }
+        if(PlayerListItems.Count < Manager.GamePlayers.Count) { CreateClientPlayerItem(); }
+        if(PlayerListItems.Count > Manager.GamePlayers.Count) { RemovePlayerItem(); }
+        if(PlayerListItems.Count == Manager.GamePlayers.Count) { UpdatePlayerItem(); }
+    }
+
+    public void FindLocalPlayer()
+    {
+        LocalPlayerObject = GameObject.Find("LocalGamePlayer");
+        LocalplayerController = LocalPlayerObject.GetComponent<PlayerObjectController>();
+    }
+
+    public void CreateHostPlayerItem()
+    {
+        foreach (var player in Manager.GamePlayers)
+        {
+            GameObject NewPlayerItem = Instantiate(PlayerListItemPrefab) as GameObject;
+            PlayerListItem NewPlayerItemScript = NewPlayerItem.GetComponent<PlayerListItem>();
+
+            NewPlayerItemScript.PlayerName = player.PlayerName;
+            NewPlayerItemScript.ConnectionID = player.ConnectionID;
+            NewPlayerItemScript.PlayerSteamID = player.PlayerSteamID;
+            NewPlayerItemScript.SetPlayerValues();
+
+            NewPlayerItem.transform.SetParent(PlayerListViewContent.transform);
+            NewPlayerItem.transform.localScale = Vector3.one;
+
+            PlayerListItems.Add(NewPlayerItemScript);
+        }
+        PlayerItemCreated = true;
+    }
+
+    public void CreateClientPlayerItem()
+    {
+        foreach (var player in Manager.GamePlayers)
+        {
+            if(!PlayerListItems.Any(b => b.ConnectionID == player.ConnectionID))
+            {
+                GameObject NewPlayerItem = Instantiate(PlayerListItemPrefab) as GameObject;
+                PlayerListItem NewPlayerItemScript = NewPlayerItem.GetComponent<PlayerListItem>();
+
+                NewPlayerItemScript.PlayerName = player.PlayerName;
+                NewPlayerItemScript.ConnectionID = player.ConnectionID;
+                NewPlayerItemScript.PlayerSteamID = player.PlayerSteamID;
+                NewPlayerItemScript.SetPlayerValues();
+
+                NewPlayerItem.transform.SetParent(PlayerListViewContent.transform);
+                NewPlayerItem.transform.localScale = Vector3.one;
+
+                PlayerListItems.Add(NewPlayerItemScript);
+            }
+        }
+    }
+
+    public void UpdatePlayerItem()
+    {
+        foreach(var player in Manager.GamePlayers)
+        {
+            foreach (var PlayerListItemScript in PlayerListItems)
+            {
+                if(PlayerListItemScript.ConnectionID == player.ConnectionID)
+                {
+                    PlayerListItemScript.PlayerName = player.PlayerName;
+                    PlayerListItemScript.SetPlayerValues();
+                }
+            }
+        }
+    }
+
+    public void RemovePlayerItem()
+    {
+        List<PlayerListItem> playerListItemToRemove = new List<PlayerListItem>();
+
+        foreach (var playerlistItem in PlayerListItems)
+        {
+            if(!Manager.GamePlayers.Any(b=> b.ConnectionID == playerlistItem.ConnectionID))
+            {
+                playerListItemToRemove.Add(playerlistItem);
+            }
+        }
+        if(playerListItemToRemove.Count > 0)
+        {
+            foreach(PlayerListItem playerlistItemToRemove in playerListItemToRemove)
+            {
+                GameObject ObjectToRemove = playerlistItemToRemove.gameObject;
+                PlayerListItems.Remove(playerlistItemToRemove);
+                Destroy(ObjectToRemove);
+                ObjectToRemove = null;
+            }
+        }
+    }
+}
---
+++ Assets/PlayerObjectController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+
+public class PlayerObjectController : NetworkBehaviour
+{
+    [SyncVar] public int ConnectionID;
+    [SyncVar] public int PlayerIdNumber;
+    [SyncVar] public ulong PlayerSteamID;
+    [SyncVar(hook = nameof(PlayerNameUpdate))] public string PlayerName;
+
+    private CustomNetworkManager manager;
+
+    private CustomNetworkManager Manager
+    {
+        get
+        {
+            if(manager != null)
+            {
+                return manager;
+            }
+            return manager = CustomNetworkManager.singleton as CustomNetworkManager;
+        }
+    }
+
+    public override void OnStartAuthority()
+    {
+        CmdSetPlayerName(SteamFriends.GetPersonaName().ToString());
+        gameObject.name = "LocalGamePlayer";
+        LobbyController.instance.FindLocalPlayer();
+        LobbyController.instance.UpdateLobbyName();
+    }
+
+    public override void OnStartClient()
+    {
+        Manager.GamePlayers.Add(this);
+        LobbyController.instance.UpdateLobbyName();
+        LobbyController.instance.UpdatePlayerList();
+    }
+
+    public override void OnStopClient()
+    {
+        Manager.GamePlayers.Remove(this);
+        LobbyController.instance.UpdatePlayerList();
+    }
+
+    [Command]
+    private void CmdSetPlayerName(string name)
+    {
+        this.PlayerNameUpdate(this.PlayerName, name);
+    }
+
+    public void PlayerNameUpdate(string OldValue, string NewValue)
+    {
+        if(isServer)
+        {
+            this.PlayerName = NewValue;
+        }
+        if(isClient)
+        {
+            LobbyController.instance.UpdatePlayerList();
+        }
+    }
+}
