9124a808293ab0e10242df61ac6851e746aea7b7
DarthCooper <loganbarber1@outlook.com>
2023-09-24 13:53:24 -0600

    Build test yet again

--- Assets/PlayerMovementController.cs
+++ Assets/PlayerMovementController.cs
@@ -33,6 +33,7 @@ public class PlayerMovementController : NetworkBehaviour
 
             if(isOwned)
             {
+                print(movement);
                 Movement();
             }
         }
