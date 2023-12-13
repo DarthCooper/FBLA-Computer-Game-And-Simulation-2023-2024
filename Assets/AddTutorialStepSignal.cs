9a12254952133cdaf66314bf7a2d30042a4e07af
DarthCooper <loganbarber1@outlook.com>
2023-12-13 16:04:33 -0700

    movement tutorial is up and running.

--- Assets/AddTutorialStepSignal.cs
+++ Assets/AddTutorialStepSignal.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class AddTutorialStepSignal : MonoBehaviour
@@ -15,18 +16,22 @@ public class AddTutorialStepSignal : MonoBehaviour
 
     public bool SignalNot;
 
+    public UnityEvent onFinish;
+
     public void addSignal()
     {
         if(signal is EnableTutorialSignal)
         {
             var script = parent.AddComponent<EnableTutorialSignal>();
             script.goOnDisable = SignalNot;
+            script.onFinish = onFinish;
         }
         if(signal is ComponentTutorialSignal)
         {
             var script = parent.AddComponent<ComponentTutorialSignal>();
             script.component = parent.GetComponent<Image>();
             script.goOnDisable = SignalNot;
+            script.onFinish = onFinish;
         }
     }
 }
